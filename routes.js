cc19efec2e9d16cab8ca5461005fb9eda07e4081
eileen <eileen.chen@ef.com>
2019-05-24 14:15:26 +0800

    add check phone mock

--- routes.js
+++ routes.js
@@ -9,5 +9,6 @@ module.exports = {
   "/omni/apigateway/api/v1/customer/GetInfoListofGroup?groupId=:id": "/groupinfos/:id",
   "/omni/apigateway/api/v1/account/authenticate?loginName=:id&*": "/teachers/:id",
   "/omni/apigateway/api/v1/user/:id/profile": "/users/:id",
-  "/omni/apigateway/api/v1/teacher/:teacher/GetSessionsofDateandSchoolCode?year=:year&month=:month&date=:date&schoolCode=*": "/sessions/:teacher-:year-:month-:date"
+  "/omni/apigateway/api/v1/teacher/:teacher/GetSessionsofDateandSchoolCode?year=:year&month=:month&date=:date&schoolCode=*": "/sessions/:teacher-:year-:month-:date",
+  "/omni/apigateway/api/v1/ilab/checkphone?phone=:phone&code=:code":"/checkphone/:phone-;code"
 }
