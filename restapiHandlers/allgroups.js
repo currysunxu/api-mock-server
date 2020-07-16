5aad7feb3f313d3893da1ea5680f1b9d5e08cb3e
nancy.zhangcr <nancy.zhangcr@ef.com>
2020-07-16 17:29:52 +0800

    Add GT mock data based on the new omni allgroups api

---
+++ restapiHandlers/allgroups.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const dbGen = require("./../data");
+
+const constructResponse = function(entityList, success, errorMsg) {
+  return {
+    isSuccess: success,
+    ResponseDatas: entityList,
+    errorMsg: errorMsg
+  };
+};
+
+module.exports = function(req, res) {
+  console.log(req.params)
+  console.log(req.query)
+  console.log(req.body)
+
+  let list = dbGen()[req.params.resourceName];
+
+  switch (req.method) {
+    case "GET":
+      list = list.filter(i => i.id == req.query.customerId).map(i => i.data);
+      res.status(200).json(constructResponse(list, true, null));
+      break;
+    case "POST":
+      res.status(200).json(constructResponse(null, true, null));
+      break;
+    default:
+      res.status(500).text("not supported http verb");
+      break;
+  }
+};
